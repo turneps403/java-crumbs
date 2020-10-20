3650b0dd35252b013fe48ec7c8b6aa01f76c8b64
turneps403 <turneps403@mail.ru>
2020-10-20 16:14:42 +0300

    - /jwt/login is able to recognize jwt and traser correct headers

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# java-crumbs
-see branches
+# Forward Auth: middleware for Traefik 
+
+There is application to play with ``Forward Auth`` middleware.
+
+It implemets only two handlers:
+0. `/jwt/auth` to authenticate JWT and if it would be successfully recognized then the value will be set to the header
+1. `/jwt/login` to create JWT after success authentication
+
+#### Useful links
+* https://github.com/auth0/java-jwt
--- src/main/java/ru/otus/jwt/resourses/Jwt.java
+++ src/main/java/ru/otus/jwt/resourses/Jwt.java
@@ -1,19 +1,18 @@
 package ru.otus.jwt.resourses;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,8 +34,11 @@ public class Jwt {
     @Value("${jwt.secretKey}")
     String SECRET_KEY;
 
-    @RequestMapping("/recognize")
-    public Map recognize(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+    @Value("${jwt.ttl}")
+    String JWT_TTL;
+
+    @RequestMapping("/auth")
+    public Map authentication(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
         httpResponse.setHeader(RESPONSE_HEADER, DEFAULT_VALUE);
         String authHeader = httpRequest.getHeader("Authorization");
         if (authHeader != null) {
@@ -44,10 +46,12 @@ public class Jwt {
             if (authHeaderArr.length > 1 && authHeaderArr[1] != null) {
                 String token = authHeaderArr[1];
                 try {
-                    Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
-                    httpResponse.setHeader(RESPONSE_HEADER, claims.get(DESIRABLE_KEY).toString());
+                    Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+                    JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
+                    DecodedJWT jwt = verifier.verify(token);
+                    httpResponse.setHeader(RESPONSE_HEADER, jwt.getClaim(DESIRABLE_KEY).asString());
                 } catch (Exception e) {
-                    logger.log(Level.WARNING, "invalid/expired token", e);
+                    logger.log(Level.WARNING, "invalid/expired token");
                 }
             } else {
                 logger.log(Level.WARNING, "Authorization token must be Bearer [token]");
@@ -56,6 +60,24 @@ public class Jwt {
             logger.log(Level.WARNING, "Authorization token wasn't provided");
         }
         return Collections.singletonMap("status", "OK");
+    }
+
+    @RequestMapping("/login")
+    public Map login(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+        httpResponse.setHeader("Authorization", "Bearer " + createJWToken("100500"));
+        return Collections.singletonMap("status", "OK");
+    }
+
+    private String createJWToken(String keyValue) {
+        long timestamp = System.currentTimeMillis();
+        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+        String token = JWT.create()
+            .withIssuer("auth0")
+            .withIssuedAt(new Date(timestamp))
+            .withExpiresAt(new Date(timestamp + Integer.parseInt(JWT_TTL)))
+            .withClaim(DESIRABLE_KEY, keyValue)
+            .sign(algorithm);
+        return token;
     }
 
 }
