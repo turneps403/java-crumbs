70a526ef415a6591020f473bcf0160c0bab05885
turneps403 <turneps403@mail.ru>
2020-10-20 17:04:26 +0300

    - thats enough

--- src/main/java/ru/otus/jwt/resourses/Jwt.java
+++ src/main/java/ru/otus/jwt/resourses/Jwt.java
@@ -7,12 +7,16 @@ import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -37,6 +41,12 @@ public class Jwt {
     @Value("${jwt.ttl}")
     String JWT_TTL;
 
+    @Value("${jwt.login.service}")
+    String LOGIN_SERVICE;
+
+    @Value("${jwt.login.port}")
+    String LOGIN_PORT;
+
     @RequestMapping("/auth")
     public Map authentication(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
         httpResponse.setHeader(RESPONSE_HEADER, DEFAULT_VALUE);
@@ -47,7 +57,7 @@ public class Jwt {
                 String token = authHeaderArr[1];
                 try {
                     Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-                    JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
+                    JWTVerifier verifier = JWT.require(algorithm).withIssuer("project1").build();
                     DecodedJWT jwt = verifier.verify(token);
                     httpResponse.setHeader(RESPONSE_HEADER, jwt.getClaim(DESIRABLE_KEY).asString());
                 } catch (Exception e) {
@@ -63,8 +73,23 @@ public class Jwt {
     }
 
     @RequestMapping("/login")
-    public Map login(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
-        httpResponse.setHeader("Authorization", "Bearer " + createJWToken("100500"));
+    public Map login(
+            HttpServletRequest httpRequest, HttpServletResponse httpResponse,
+            @RequestParam String login, @RequestParam String password
+    ) {
+        String url = "http://{service}:{port}/login/lookup";
+        Map<String, String> urlParams = new HashMap<>();
+        urlParams.put("service", LOGIN_SERVICE);
+        urlParams.put("port", LOGIN_PORT);
+        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(url)
+            .queryParam("login", login)
+            .queryParam("password", password);
+
+        RestTemplate restTemplate = new RestTemplate();
+        Map result = restTemplate.getForObject(urlBuilder.buildAndExpand(urlParams).toUriString(), Map.class);
+        logger.log(Level.WARNING, "FOOOOOOOO:   " + result.get("user"));
+
+        httpResponse.setHeader("Authorization", "Bearer " + createJWToken(result.get("user").toString()));
         return Collections.singletonMap("status", "OK");
     }
 
@@ -72,7 +97,7 @@ public class Jwt {
         long timestamp = System.currentTimeMillis();
         Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
         String token = JWT.create()
-            .withIssuer("auth0")
+            .withIssuer("project1")
             .withIssuedAt(new Date(timestamp))
             .withExpiresAt(new Date(timestamp + Integer.parseInt(JWT_TTL)))
             .withClaim(DESIRABLE_KEY, keyValue)
---
+++ src/main/java/ru/otus/jwt/resourses/TestLogin.java
@@ -0,0 +1,20 @@
+package ru.otus.jwt.resourses;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping(path = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TestLogin {
+    @RequestMapping("/lookup")
+    public Map testtest(@RequestParam String login, @RequestParam String password) {
+        int uid = ThreadLocalRandom.current().nextInt();
+        return Collections.singletonMap("user", String.valueOf(uid));
+    }
+}
